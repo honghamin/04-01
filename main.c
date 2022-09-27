0171aa9615605a82f5d10a8d5eb9e3a28c99e6fd
honghamin <113421639+honghamin@users.noreply.github.com>
2022-09-27 16:49:29 +0900

    04-03

--- main.c
+++ main.c
@@ -4,18 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x, y;
-	int a,b,c,d,e;
+	int x;
+	int a, b;
 	
-	printf("input two integers : ");
-	scanf("%i %i", &x, &y);
+	printf("input the second : ");
+	scanf("%i", &x);
 	
-	a=x+y;
-	b=x-y;
-	c=x*y;
-	d=x/y;
-	e=x%y;
+	a=x/60;
+	b=x%60;
 	
-	printf("+ result is %d \n- result is %d \n* result is %d \n/result is %d \n%% result is %d\n", a, b, c, d, e);
+	printf("%i second is %i : %i\n", x, a, b);
 	return 0;
 }
